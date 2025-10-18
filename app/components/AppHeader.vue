4ac7af45b0b2e303593f324b03a1bd226ecd00c1
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2025-10-18 13:44:04 -0400

    wip

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="w-full max-w-[1240px] mx-auto py-16">
+        <section class="flex justify-between items-center">
+
+            <AppLogo />
+
+            <NuxtLink class="text-white flex items-center gap-2 bg-red">
+                <AppIconHeart />
+
+                Lista de favoritos
+            </NuxtLink>
+
+        </section>
+
+        <section>
+            <h1 class="font-bold text-5xl">
+                Saiba tudo em um só <span class="text-[#11B0C8]">lugar.</span>
+            </h1>
+
+            <p>Personagens, localizações, episódiose muito mais.</p>
+        </section>
+    </header>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="bg-[#1E1E1E] text-white">
+    <AppHeader />
+
     <slot />
 
     <AppFooter class="mt-20"/>
